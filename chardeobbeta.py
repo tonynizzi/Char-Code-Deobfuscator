4dd45990fd4632dd3f43fbfa5ad080d00d5cb843
tonynizzi <tony.nizzi@gmail.com>
2018-06-19 09:21:12 -0500

    Add files via upload

---
+++ chardeobbeta.py
@@ -0,0 +1,21 @@
+# This little program will take a list of char codes and convert to ASCII.
+# Input should appear like the following: char(108) + char(119) etc.
+import re
+
+print ("For pasting the char list, the input needs to appear as char(118)+char(14) or char(65)&char(59) etc...")
+
+# This takes the users char list.
+first_list = input("Please paste your char list: ")
+
+# This splits up all of the individual chars into a list by separating at the "+ and the &. Expandable as needed".
+second_list = re.split('\&+|\++', first_list)
+
+# This removes all of the extra characters with Regex, converts to ASCII and rebuilds the line.
+def convert_code():
+    for line in second_list:
+        line = re.sub(r'\D', "", line)
+        line = chr(int(line))
+        print(line, end="")
+
+# Run the function.
+convert_code()
